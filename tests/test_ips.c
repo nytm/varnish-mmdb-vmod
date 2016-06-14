128972bc2df6353bd2d41ff0268752c113b47db3
Russell Simpkins <russell.simpkins@nytimes.com>
2016-06-14 15:20:23 -0400

    removed blank spaces and updated to work

--- tests/test_ips.c
+++ tests/test_ips.c
@@ -1,4 +1,3 @@
-#define DEBUG 0
 #include <ctype.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -13,7 +12,7 @@
 // -m CityIP2.mmdb file
 // -d file_of_errors.csv - for all instances where there were 3 or more - in the lookup result
 // -o file_of_ip_lookup.csv - file to see what we got back from maxmind
-// again, this is a utility script for diagnosing what comes back from 
+// again, this is a utility script for diagnosing what comes back from
 // maxmind
 
 static char* MMDB_CITY_PATH = MAX_CITY_DB;
@@ -42,11 +41,11 @@ int main(int argc, char **argv) {
 			if (optopt == 'm' || optopt == 'i' || optopt == 'o' || optopt == 'd')
 				fprintf(stderr, "Option -%c requires you give the location of the file to use.\n", optopt);
 			else if (isprint (optopt))
-               fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-             else
-               fprintf (stderr,
-                        "Unknown option character `\\x%x'.\n",
-                        optopt);
+			   fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+			 else
+			   fprintf (stderr,
+						"Unknown option character `\\x%x'.\n",
+						optopt);
 		default :
 			return 1;
 		}
@@ -66,8 +65,8 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	MMDB_s *mmdb_handle = NULL;
-    int mmdb_baddb = open_mmdb(mmdb_handle);
+	MMDB_s mmdb_handle;
+	int mmdb_baddb = open_mmdb(&mmdb_handle);
 	if (mmdb_baddb) {
 		fprintf(stderr, "There was a problem opening %s\n", MMDB_CITY_PATH);
 		return 1;
@@ -89,7 +88,7 @@ int main(int argc, char **argv) {
 
 	// dump function will open this file when we call it.
 	fclose(debug);
-		
+
 	FILE *output = NULL;
 	if (outputfile != NULL) {
 		output = fopen(outputfile, "w+");
@@ -98,15 +97,14 @@ int main(int argc, char **argv) {
 			fclose(f);
 			return errno;
 		}
-		
+
 	}
-	
 	char *ipaddr;
 	ipaddr = calloc( 255, sizeof(char) );
 	do {
 
 		ipaddr = fgets(ipaddr,255,f);
-		if (ipaddr == NULL) 
+		if (ipaddr == NULL)
 			break;
 
 		// consume the new line
@@ -114,28 +112,28 @@ int main(int argc, char **argv) {
 		if (c != NULL) {
 			*c = '\0';
 		}
-		c = geo_lookup_weather(mmdb_handle, ipaddr, 0);
+		c = geo_lookup_weather(&mmdb_handle, ipaddr, 0);
 		int index, end, count;
 
 		count = 0;
 		end   = strlen(c);
-		// if there's more than 2 - in the result, let's dump it to file 
+		// if there's more than 2 - in the result, let's dump it to file
 		// for closer examination.
 		for (index = 0; index < end; ++index) {
 			if (c[index] == '-')
 				count+=1;
 			if (count > 2) {
-				dump_failed_lookup(mmdb_handle, ipaddr, debugfile);
+				dump_failed_lookup(&mmdb_handle, ipaddr, debugfile);
 				break;
 			}
 		}
-		if (output == NULL) 
+		if (output == NULL)
 			fprintf(stdout, "%s,%s\n", ipaddr, c);
 		else
 			fprintf(output, "%s,%s\n", ipaddr, c);
 
 	} while (ipaddr != NULL);
 	free(ipaddr);
-	close_mmdb(mmdb_handle);
+	close_mmdb(&mmdb_handle);
 	fclose(f);
 }
